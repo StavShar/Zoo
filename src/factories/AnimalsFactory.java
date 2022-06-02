f9530750f7c930d202efd148063c0552d003bd74
StavShar <stavshar21@gmail.com>
2022-06-02 04:23:38 +0300

    factories files

---
+++ src/factories/AnimalsFactory.java
@@ -0,0 +1,15 @@
+package factories;
+
+import animals.*;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import static animals.Animal.*;
+import static animals.Animal.validHorSpeed;
+
+public class AnimalsFactory{
+
+}
---
+++ src/factories/OmnivoreFactory.java
@@ -0,0 +1,10 @@
+package factories;
+
+import animals.Animal;
+import animals.Bear;
+import diet.Omnivore;
+import mobility.Point;
+
+public class OmnivoreFactory {
+
+}
